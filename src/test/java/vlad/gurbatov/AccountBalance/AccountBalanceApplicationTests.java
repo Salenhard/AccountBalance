de7c77a7312c1aae2ab0ad4338c51b152ea4b830
Salenhard <bratiski@mail.ru>
2025-06-17 17:44:08 +1000

    first commit

---
+++ src/test/java/vlad/gurbatov/AccountBalance/AccountBalanceApplicationTests.java
@@ -0,0 +1,13 @@
+package vlad.gurbatov.AccountBalance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountBalanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
